5cca3c698b47a911e0f0c3211241ebdf0360c5dd
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-07-26 02:26:18 -0500

    updating user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :posted_recipes, class_name: "Recipe"
+  has_many :ingredients, through: :recipes
+  has_many :comments, foreign_key: :commenter_id
+  has_many :replies, foreign_key: :replier_id
+  has_many :favorite_recipes
+  has_many :recipes_favorited, through: :favorite_recipes, source: :recipe
 end
