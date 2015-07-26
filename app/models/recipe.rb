9e76f455c7d34647b382d0cc9f07c1989ff26495
Durkheim <mosesyk.dev.bootcamp@gmail.com>
2015-07-26 04:35:50 -0500

    adding has_many relationship from recipe to ingredients, photos, and directions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,7 @@ class Recipe < ActiveRecord::Base
   has_many :commenters, through: :comments
   has_many :favorite_recipes
   has_many :users_who_favorited, through: :favorite_recipes, source: :user
+  has_many :ingredients
+  has_many :directions
+  has_many :recipe_photos
 end
